2e082866c5cfd52623806fa82b49c0eac04f75f5
Ar0312yan <145320197+Ar0312yan@users.noreply.github.com>
2025-04-21 17:14:26 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include "mem_leak_detector.h"
+
+int main() {
+    atexit(report_leaks);  // Call at program exit
+
+    int* a = malloc(sizeof(int) * 5); // Not freed (intentional leak)
+    char* b = malloc(20);
+    free(b);
+
+    return 0;
+}
 
