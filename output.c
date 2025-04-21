6bd8a1b90c7c07ee624e7619302afe79b7cfb7e3
Ar0312yan <145320197+Ar0312yan@users.noreply.github.com>
2025-04-21 20:49:10 +0530

    Update output.c

--- output.c
+++ output.c
@@ -1,1 +1,16 @@
+oaded 'C:\WINDOWS\SysWOW64\kernel32.dll'. Symbols loaded.
+Loaded 'C:\WINDOWS\SysWOW64\KernelBase.dll'. Symbols loaded.
+Loaded 'C:\WINDOWS\SysWOW64\apphelp.dll'. Symbols loaded.
+Loaded 'C:\WINDOWS\SysWOW64\msvcrt.dll'. Symbols loaded.
+[Warning] Double free detected at C:\Users\aryan\OneDrive\Desktop\LeakGuard\main.c:22 (originally freed at C:\Users\aryan\OneDrive\Desktop\LeakGuard\main.c:20)
+[Warning] Attempt to free untracked pointer at C:\Users\aryan\OneDrive\Desktop\LeakGuard\main.c:26
+Leak: 20 bytes from C:\Users\aryan\OneDrive\Desktop\LeakGuard\main.c:8
+Leak: 20 bytes from C:\Users\aryan\OneDrive\Desktop\LeakGuard\main.c:7
+Total allocated: 128 bytes
+Total freed:     88 bytes
+Memory leaked:   40 bytes
+Memory leaks found! ⚠️
+=thread-exited,id="1",group-id="i1"
+The program 'C:\Users\aryan\OneDrive\Desktop\LeakGuard\main.exe' has exited with code 0 (
+
 
